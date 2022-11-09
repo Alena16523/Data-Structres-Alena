a6e32048f2e5b0f6322b753daba6254ee45f439d
Alena Elhajassaad <alenaelhajassaad@yahoo.com>
2022-11-08 20:46:57 -0500

    started Array algorithm

---
+++ 01.AlgorithmAnalysis/src/main/java/ArrayDemo.java
@@ -0,0 +1,14 @@
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+
+        int[] scoreArray=new int[10];
+        System.out.println(scoreArray); //prints starting address in the memory
+
+
+
+
+    }
+
+
+}
